87c4ed87d948da0b6393ea7bf5ff8ac6f0a1167a
Lukas Leindals <s183920@student.dtu.dk>
2023-07-24 23:09:54 +0200

    add pokemon text

--- alternative_boardgames/pokemon_narrator.py
+++ alternative_boardgames/pokemon_narrator.py
@@ -8,54 +8,289 @@ cells = {
     },
     1 : {
         "name" : None,
-        "text" : None,
+        "text" : "Rattata used Tackle! ...wait, you seriously rolled a 1? You fainted. Finish your drink.",
     },
     2 : {
         "name" : None,
-        "text" : None,
+        "text" : "Pidgey used Quick Attack! Use that quickness to give 1 drink and take and extra turn.",
     }, 
     3 : {
         "name" : None, 
-        "text" : None,
+        "text" : "Caterpie used String Shot! It was super effective! All other players may only move 1/2 of waht they roll on their next turn (round up).",
     },
     4 : {
         "name" : None,
-        "text" : None,
+        "text" : "You caught a Pikacgu! Drink 2 and replace your starter with this electric franchise.",
     },  
     5 : {
         "name" : None,
-        "text" : None,
+        "text" : "Beedrill used Twineedle! Pick two people to drink.",
     },
     6 : {
         "name" : "Pewter Gym",
-        "text" : None,
+        "text" : "Roll a die. Even: Give a drink. Odd: Take a drink.",
     },
     7 : {
         "name" : None,
-        "text" : None,
+        "text" : "If you're a guy, guys drink. If you're a girl, girls drink.",
     },
     8 : {
         "name" : None,
-        "text" : None,
+        "text" : "Zubats... they're everywhere! Take a drink. Next turn, if you roll a 1 or 2, stay here and take a drink!",
     },
     9 : {
         "name" : None,
-        "text" : None,
+        "text" : "Clefairy used Metronome! Close your eyes, point to a random square, and drink or give what it says. If no drink is given or taken, just drink 2.",
     },
     10 : {
         "name" : None,
-        "text" : None,
+        "text" : "Jigglypuff used Sing! Everyone else fell asleep. Take an extra turn.",
     },
     11 : {
         "name" : None,
-        "text" : None,
+        "text" : "Abra used Teleport! Teleport to the other Abra.",
     },
     12 : {
         "name" : "Gary",
-        "text" : None,
+        "text" : "Roll a die. Drink half, give half (round up). What's this guy's deal, anyway?",
     },
     13 : {
         "name" : "Cerulean Gym",
+        "text" : "Misty's water attacks caused splash damage. You drink 2, everyone else drinks 1.",
+    },
+    14 : {
+        "name" : None,
+        "text" : "Slowpoke is slow. For the first one here, make up a gesture. For the rest of the game, when you do it, the last to mimic you takes a drink.",
+    },
+    15 : {
+        "name" : None,
+        "text" : "Bellsprout used Razor Leaf! Shred someone's dignity with a reckless callout. They drink 1 in shame.",
+    },
+    16 : {
+        "name" : None,
+        "text" : "Meowth used Pay Day! Everybody but you takes a drink.",
+    },
+    17 : {
+        "name" : None,
+        "text" : "Diglet used Dig! Dig deep and finish you drink.",
+    },
+    18 : {
+        "name" : None,
+        "text" : "Enjoy your cruise aboard the S.S. Anne! Roll a die, you lose that many turns aboard the luxury cruise liner. Roll again, and drink that number during each lost turn.",
+    },
+    19 : {
+        "name" : "Vermilion Gym",
+        "text" : "Roll a die. Even, you're paralyzed; take 2 drinks and miss your next turn. Odd,  take a drink.",
+    },
+    20 : {
+        "name" : None,
+        "text" : "I want to ride my BICYCLE! BICYCLE! BICYCLE! On your next turn, roll the die and move twice that number.",
+    },
+    21 : {
+        "name" : None,
+        "text" : "Magikarp used Spash! ...but nothing happnened...",
+    },
+    22 : {
+        "name" : None,
+        "text" : "Sandshrew used Sand-Attack! Your accuracy is lowered. For the rest of the game, you may only drink with your non-dominant hand.",
+    },
+    23 : {
+        "name" : "Pokemon Tower",
+        "text" : "While in the Pokemon Tower, out of respect for the dead, you should not speak. Doing so results in a drink eah time. Take a drink noe for your fallen comrades.",
+    },
+    24 : {
+        "name" : None,
+        "text" : "A possesed Channeler. Now you're possessed too! While you are on this space, anyone may make you get them a drink.",
+    },
+    25 : {
+        "name" : None,
+        "text" : "Haunter used Dream Eater! Devour someone else's dreams by moving them back 10 spaces.",
+    },
+    26 : {
+        "name" : None,
+        "text" : "Cubone used 'My mother is dead.' Share a depressing story with the group. Then everyone take a drink.",
+    },
+    27 : {
+        "name" : None,
+        "text" : "If someone is in Silph Co, you use the Silph Scope to beat the ghost and everyone else drinks. Otherwise take 3 drinks to appease the dead.",
+    },
+    28 : {
+        "name" : None,
+        "text" : "Abra used Teleport! Teleport to the other Abra.",
+    },
+    29 : {
+        "name" : None,
+        "text" : "A sleeping Snorlax blocks your path. Belt out a song of the group's choice to wake him, or take 4 drinks.",
+    },
+    30 : {
+        "name" : "Gary",
+        "text" : "Roll a die. Drink that number minus one. Seriously though, is this dude following you or something?",
+    },
+    31 : {
+        "name" : None,
+        "text" : "Eeveelution time! You choose a new rule! Any rule violation result in a drink.",
+    },
+    32 : {
+        "name" : "Celadon Gym",
+        "text" : "Roll a die. 1 to 3: Stun Spore; Lose a turn. 4 to 6: Mega Drain; Finish your drink.",
+    },
+    33 : {
+        "name" : None,
+        "text" : "Psyduck is slow. For the first one here, make up a gesture. For the rest of the game, when you do it, the last to mimic you takes a drink.",
+    },
+    34 : {
+        "name" : None,
+        "text" : "What? Your Pokemon is evolving! Let it evolve: Drink 4 and skip the next gym. Stop evelution: Take an extra turn.",
+    },
+    35 : {
+        "name" : None,
+        "text" : "Porygon used Tri Attack! While on this space, for each drink you are given, the giver must drink 3.",
+    },
+    36 : {
+        "name" : "Silph Co.",
+        "text" : "You've infiltrated the headquarters of the infamous Team Rocket! You will need all your courage to make it to their leader. Drink an extra 2 each turn to calm your nerves.",
+    },
+    37 : {
+        "name" : None,
+        "text" : "A scientist uses his magnet Pokemon! You magnetically attract 1 drink per player in the game.",
+    },
+    38 : {
+        "name" : None,
+        "text" : "Lapras used Confuse Ray! Pick a player; they are now confused. Next turn, they must roll a 1 to 3 to stop being confused. If not, they are still confused and lose a turn.",
+    },
+    39 : {
+        "name" : None,
+        "text" : "It's Team Rocket! Watch them defeat themselves with incompetence, and everyone drink to them blasting off.",
+    },
+    40 : {
+        "name" : "Giovanni",
+        "text" : "Roll a die. 1 to 3: Give that number. 4 to 6: Drink that number.",
+    },
+    41 : {
+        "name" : None,
+        "text" : "Rare Candy - Level up! You get an extra turn.",
+    },
+    42 : {
+        "name" : "Gary",
+        "text" : "Roll a die and take that many drinks. The next time this punk hassles you will be the last.",
+    },
+    43 : {
+        "name" : "Saffron Gym",
+        "text" : "Use psychic powers to pick a number, then roll the die. If it's your number, take an extra turn. If not, drink 2.",
+    },
+    44 : {
+        "name" : None,
+        "text" : "Challenge someone to a chugging contest. First to finish gets an extra turn, last to finish loses a turn.",
+    },
+    45 : {
+        "name" : None,
+        "text" : "Crabby used Crabhammer! Bring down the Crabhammer on someone; they must finish their drink.",
+    },
+    46 : {
+        "name" : "Ditto used transform! During the next person's turn, you must copy everything they do.",
+        "text" : None,
+    },
+    47 : {
+        "name" : None,
+        "text" : "Doduo used Double-Edge! You give 4 drinks, but you drink 1.",
+    },
+    48 : {
+        "name" : "Safari Zone",
+        "text" : "Before each turn in the Safari Zone, roll a die. 1 to 2: You throw bait. Give 1 drink to someone. 3 to 4: You throw a rock, dick. Lose your turn, drink 4. 5 to 6: You throw a safari ball. Drink 2 in sadness, because safari balls are just awful."
+    },
+    49 : {
+        "name" : None,
+        "text" : "Gone fishin'... a wild Dratini appeared! Roll a 1 to catch. Otherwise, drink 1.",
+    },
+    50 : {
+        "name" : None,
+        "text" : "A wild Taurus appeared... but instantly fled. Drink 2 for not being quick enough.",
+    },
+    51 : {
+        "name" : None,
+        "text" : "Roll the die. If it's 1 to 3 Chansey eludes you, drink 1. If 4 to 6, you capture Cansey, give 2."
+    },
+    52 : {
+        "name" : "Fuchsia Gym",
+        "text" : "Poison Pokemon are Toxic! Better get intoxicated! Drink 3.",
+    },
+    54 : {
+        "name" : None,
+        "text" : "Electrode used Explosion! Everybody finish their drinks!",
+    },
+    55 : {
+        "name" : None,
+        "text" : "Electabuzz used Thunder Punch! You're paralyzed; miss your next turn.",
+    },
+    56 : {
+        "name" : None,
+        "text" : "Poliwag used Hydro Pump! Shotgun a beer.",
+    },
+    57 : {
+        "name" : None,
+        "text" : "Seaking used Waterfall! ...do a waterfall!",
+    },
+    58 : {
+        "name" : None,
+        "text" : "A wild Missingno! Roll 3 times. Get a 5 or 6, and you continue. If not, you glitched. Restart at Pallet Town.",
+    },
+    59 : {
+        "name" : "Cinnabar Gym",
+        "text" : "Roll a die. Even, roll again. Odd, drink twice as many times as you rolled evens."
+    },
+    60 : {
+        "name" : None,
+        "text" : "Koffing used Haze! If there's anything nearby to smoke, smoke it to avoid taking 2 drinks.",
+    },
+    61 : {
+        "name" : None,
+        "text" : "You ressurected a Fossil Pokemon! Everyone older than you drinks 2.",
+    },
+    62 : {
+        "name" : None,
+        "text" : "You throw a Pokeball! If your favorite Pokemon is on the board, roll a 1 to 3 to catch it! Roll a 4 to 6 and it got away, drink 3. If your favorite is not on the board, sadly drink 3.",
+    },
+    63 : {
+        "name" : None,
+        "text" : "Persion used Fury Swipes! Roll a die, and give out that many drinks.",    
+    },
+    64 : {
+        "name" : "Viridian Gym",
+        "text" : "First, take a drink. Then, if you're a guy, guys take 3. If you're a girl, girls take 3.",
+    },
+    65 : {
+        "name" : None,
+        "text" : "Fearow used Mirror Move! Drink what the last person did during his/her turn.",
+    },
+    66 : {
+        "name" : None,
+        "text" : "Graveler used Defense Curl! Lose 2 turns, but you do not have to take any drinks until you go again.",
+    },
+    67 : {
+        "name" : None,
+        "text" : "Gyrados used Dragon Rage! Take 4 drinks unless you landed on Magikarp, in which case you give 4 drinks.",
+    },
+    68 : {
+        "name" : None,
+        "text" : "Dragonite used Hyper Beam! Give 5 drinks, but lose a turn to recharge.",
+    },
+    69 : {
+        "name" : None,
+        "text" : "Gotta catch 'em all! Roll a die. 1 to 3: Why are you throwing Great Balls at it? Take a drink! 4 to 6: You got one! You may only move on once you've caught all 3 birds.",
+    },
+    70 : {
+        "name" : "The Elite Four",
+        "text" : "Challenge the land's greatest trainers: the Elite Four! Roll a 4 to defeat the Elite Four! For any other number, drink 4!",
+    },
+    71 : {
+        "name" : "Champion Gary",
+        "text" : "Finish a full drink to take down this bastard for the last time! You cannot move on until your drink is finished.",
+    },
+    72 : {
+        "name" : "Pokemon Master",
+        "text" : "Throw a Master Ball and take a victory drink. All other players toast to your glory! You are a Pokemon Master!",
+    73 : {
+        "name" : None,
         "text" : None,
     },
 }
